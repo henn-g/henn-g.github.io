1b5a0b8205c7a5646d875dc4b99d2cc6e2374ba7
henn-g <haguerra@ucsd.edu>
2019-11-19 17:37:23 -0800

    base project skeleton

---
+++ src/main.go
@@ -0,0 +1,23 @@
+package main
+
+
+/*
+ Simple server to host my portfolio site
+ Capabilities:
+		- host simple static pages with images
+		- design in context for 127 class
+
+ Stretch Goals:
+		- Implement "contact me" functionality
+		- Track who visits site, collect / harvest 
+			 relevant data
+		- Put current RELEVANT projects on site
+		- (VERY STRETCH) Short, quick blog posts 
+			(like things I learn about golang / swift along the way)
+
+	Outstanding Problems:
+		-
+*/
+func main() {
+
+}
