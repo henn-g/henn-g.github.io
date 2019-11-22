5e5e10580548fcb2363f509480e8efe6a97832f8
henn-g <haguerra@ucsd.edu>
2019-11-21 16:28:23 -0800

    only want to push index html for now

--- src/main.go
+++ src/main.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	"log"
+	"net/http"
+	"io/ioutil"
+)
 
 /*
  Simple server to host my portfolio site
@@ -11,6 +16,7 @@ package main
 		- Implement "contact me" functionality
 		- Track who visits site, collect / harvest 
 			 relevant data
+		- Design an API for other side projects
 		- Put current RELEVANT projects on site
 		- (VERY STRETCH) Short, quick blog posts 
 			(like things I learn about golang / swift along the way)
@@ -18,6 +24,29 @@ package main
 	Outstanding Problems:
 		-
 */
-func main() {
 
+func main() {
+	var fileServer = http.FileServer(http.Dir("templates"))
+	http.Handle("/", fileServer)
+
+	log.Println("Server up...")
+	http.ListenAndServe(":8080", nil)
 }
+
+// RootHandler is the base url router handler 
+type RootHandler struct { }
+
+func (rootHandler *RootHandler) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	var urlPath = req.URL.Path[1:]
+	log.Println(urlPath)
+
+	var data, err = ioutil.ReadFile(string(urlPath))
+
+	if err == nil {
+		w.Write(data)
+	} else {
+		w.WriteHeader(404)
+		w.Write([]byte("404 !" + http.StatusText(404)))
+	}
+}
+
